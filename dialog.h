73f271139213c4e4e2435c6b6bbbc9abd2668461
Flazher <flazhik@gmail.com>
2013-08-12 17:37:29 +0000

    I FORGOT dialog.h, MORON

---
+++ dialog.h
@@ -0,0 +1,25 @@
+#define BB_DIALOG_SOCKET_CREATION_ERROR	"Can't create socket"
+#define BB_DIALOG_LISTENER_BIND_ERROR	"Can't bind listener"
+
+#define BB_DIALOG_CONNECTION_FAIL		"Can't connect to server"
+#define BB_DIALOG_CONNECTION_SUCCESSFUL	"Connection successful.\nPress any key to initialize curses interface\n"
+#define BB_DIALOG_SIZE_REQUEST			"OK. Now we need to define size of matrix.\nIt must be a positive number which less than 16 (not including).\nSize: "
+#define BB_DIALOG_WARNING				"Warning"
+#define BB_DIALOG_REQUEST_IGNORED		"Request ignored by server"
+#define BB_DIALOG_SIGNATURE_MISMATCH	"Signature mismatches"
+#define BB_DIALOG_FIELD_RECEIVED		"Game field received"
+#define BB_DIALOG_FIELD_SIZE_MISMATCH	"Matrix size mismatches! Packet may be corrupted. Exit."
+#define BB_DIALOG_CANT_MOVE				"You can't move this way"
+#define BB_DIALOG_YOU_WON				"You won!"
+
+
+#define BB_DIALOG_AWAIT_FOR_CONNECTION	"Awaiting for connections"
+#define BB_DIALOG_AWAIT_FOR_INIT		"Awaiting for init command\n"
+#define BB_DIALOG_CONNECTION_FROM		"Connection from %s"
+#define BB_DIALOG_GENERATING_FIELD		"Generating game field with %ix%i matrix\n"
+#define BB_DIALOG_STEPS					"Client makes %i step"
+#define BB_DIALOG_CONNECTION_REFUSED	"Connection refused by %s"
+
+#define BB_DIALOG_TOO_LARGE_PACKET		"Too large packet has been received"
+#define BB_DIALOG_STRANGE_PACKET		"Strange packet has been received"
+
